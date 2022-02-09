ae5b5eca1241818cd9e468207b8fcc835099b0a7
VishSeen <vishseenarain@gmail.com>
2022-02-09 09:11:37 +0400

    Basic styled compoenents DONE

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
+
+
+
+  /**
+   * USING STYLED COMPONENTS
+   * Styled components are just basic css that you can use directly to create a component.
+   */
+
+
+  // Simple title with an H1 component
+  const Title = styled.h1`
+    color: white;
+    font-size: 4rem;
+  `;
+
+
+
+  // Basic button component with props
+  const Button = styled.button`
+    font-size: 1.2rem;
+    margin: 1em;
+    padding: 1rem 2rem;
+    color: ${props => props.White ? "white" : 'lightblue'}; 
+    border-color: ${props => props.White ? "white" : 'lightblue'};
+    border: 2px solid;
+    border-radius: 3px;
+    background: transparent;
+    cursor: pointer;
+  `;
+
+  // Extending basic button for more styling
+  const YellowButton = styled(Button)`
+    border-color: yellow;
+    border-radius: 10px;
+    color: yellow;
+    font-size: 1rem;
+    text-transform: uppercase;
+  `;
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* Using the styled components */}
+        <Title>Styled</Title>
+
+        <Button>Basic Button</Button>
+
+        <Button White>White button</Button>
+
+        <YellowButton>Extended Button</YellowButton>
       </header>
     </div>
   );
